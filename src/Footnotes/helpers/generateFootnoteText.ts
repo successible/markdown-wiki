1a5fb05f8e6724859e9298f6d022d0afa51a3822
Paul <paul@paulzakin.com>
2023-05-02 22:39:38 -0500

    Update

--- src/Footnotes/helpers/generateFootnoteText.ts
+++ src/Footnotes/helpers/generateFootnoteText.ts
@@ -1,4 +1,4 @@
 export const generateFootnoteText = () => {
-  const replacement = `[^${Math.random().toString(18).slice(2)}]`
+  const replacement = `[^${Math.random().toString(8).slice(2)}]`
   return replacement
 }
