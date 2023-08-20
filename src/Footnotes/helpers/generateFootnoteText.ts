c331466ae33cbd5e268a9db128696cad5b7b09d5
Paul Zakin <paul@paulzakin.com>
2023-08-20 15:35:02 -0400

    Generate footnotes with truncated uuid vs. Math.random (#108)

--- src/Footnotes/helpers/generateFootnoteText.ts
+++ src/Footnotes/helpers/generateFootnoteText.ts
@@ -1,4 +1,6 @@
+import { v4 as uuid } from 'uuid'
+
 export const generateFootnoteText = () => {
-  const replacement = `[^${Math.random().toString(18).slice(11)}]`
+  const replacement = `[^${uuid().replace(/-/g, '').slice(0, 5)}]`
   return replacement
 }
